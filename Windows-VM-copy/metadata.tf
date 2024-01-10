6b893e7c8d370752083ed42b4a4a9e89c7ce63b0
Godswill Ekpe <132562475+geenicetech@users.noreply.github.com>
2024-01-10 21:19:10 +0100

    Create metadata.tf

---
+++ Windows-VM-copy/metadata.tf
@@ -0,0 +1,10 @@
+"itemDisplayName": "Deploy a simple Windows VM Count to Azure"
+
+"description": "Deploys a simple Windows VM Count."
+
+"summary": "This template also deploys a Resource group, Storage account, Storage container, Virtual network (with one subnet), 
+ Public IP, Network Security Group and a Network Interface."
+
+"githubUsername": "geenicetech"
+
+"dateUpdated": "10-01-2024"
